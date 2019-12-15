07783ba261f728cf3ffe58dd0d65b9e0de939809
cedeerwe <cdominik@gmail.com>
2019-12-15 22:57:20 +0100

    Updates

---
+++ precache-manifest.1bb8bb2cc8ad1414374025f114f68635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5555f3f98540ae5323b6c0d414c716ee",
+    "url": "/christmas-auction/index.html"
+  },
+  {
+    "revision": "9f0af5a8f850f1dba029",
+    "url": "/christmas-auction/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9f0af5a8f850f1dba029",
+    "url": "/christmas-auction/static/js/2.13abc0bd.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/christmas-auction/static/js/2.13abc0bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d115fb35083aa8296cd",
+    "url": "/christmas-auction/static/js/main.a6d3d0ca.chunk.js"
+  },
+  {
+    "revision": "e939e13c4531974a79b1",
+    "url": "/christmas-auction/static/js/runtime-main.7ad1abf8.js"
+  }
+]);
